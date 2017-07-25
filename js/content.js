df2a6a3a8464d196a78f07af10da96a706347efa
mybookmark <mybookmark2017@gmail.com>
2017-07-25 22:55:23 +0800

    Update content.js

--- js/content.js
+++ js/content.js
@@ -66,18 +66,18 @@
 				var varPubDate = $(allItemList[j]).find('pubDate').text();				
 				var author =getAuthor(varLink);
 				
-				//if (varDescription.indexOf("</div") !== -1)
+				if (varDescription.indexOf("</div") !== -1)
 				{
-				  // varDescription = varDescription.substring(0, varDescription.indexOf("</div>")) +"</div>";
-				   varDescription = varDescription.replace(/<strong>/g,'');
-				   varDescription = varDescription.replace(/<\/strong>/g,'');
-				                                 
-				   varDescription = varDescription.replace(/<p.../g,'');
-				   varDescription = varDescription.replace(/.../g,'...</p>');
-				  
+				   varDescription = varDescription.substring(0, varDescription.indexOf("</div>")) +"</div>";
 				}
-				console.log("--------------------------------------");
-				console.log(varDescription);
+				varDescription = varDescription.replace(/<strong>/g,'');
+				varDescription = varDescription.replace(/<\/strong>/g,'');
+				                                 
+				varDescription = varDescription.replace(/<p.../g,'');
+				//varDescription = varDescription.replace(/.../g,'...</p>');
+				  
+				
+				
 				
 				var rowType ="odd";
 				if (j%2 == 0)
